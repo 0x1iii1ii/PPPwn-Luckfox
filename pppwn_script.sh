aec253547135c98cba6a3c2d80e75a393ef6f4b7
0x1iii1ii <hokseng06@gmail.com>
2024-05-25 17:52:04 +0700

    update script

---
+++ pppwn_script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Disable eth0
+ifconfig eth0 down
+# Wait a seconds
+sleep 1
+# Enable eth0
+ifconfig eth0 up
+# Wait a seconds
+sleep 1
+# Change to the directory containing the pppwn executable
+cd /home/pico/PPPwn-Luckfox/
+# Execute the pppwn command with the desired options
+./pppwn --interface eth0 --fw 1100 --stage1 "stage1.bin" --stage2 "stage2.bin" -a
+
+# Check if the pppwn command was successful
+if [ $? -eq 0 ]; then
+    echo "pppwn execution completed successfully."
+    systemctl stop pppwn.service
+else
+    echo "pppwn execution failed. Exiting script."
+    exit 1
+fi
+
