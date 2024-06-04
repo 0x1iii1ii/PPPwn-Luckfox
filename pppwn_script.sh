3975e5ae689c4ab83b69af550439971924d980b6
0x1iii1ii <hokseng06@gmail.com>
2024-06-04 22:21:54 +0700

    reduce crash, update pppwn

--- pppwn_script.sh
+++ pppwn_script.sh
@@ -15,12 +15,14 @@ sleep 1
 # Change to the directory containing the pppwn executable
 cd /home/pico/PPPwn-Luckfox/
 # Execute the pppwn command with the desired options
-./pppwn --interface eth0 --fw 1100 --stage1 "stage1.bin" --stage2 "stage2.bin" -a -t 10 -nw -rs
+./pppwn --interface eth0 --fw 1100 --stage1 "stage1.bin" --stage2 "stage2.bin" -a -t 5 -nw -wap 2
 
 # Check if the pppwn command was successful
 if [ $? -eq 0 ]; then
     echo "pppwn execution completed successfully."
     systemctl stop pppwn.service
+	sleep 20
+	ifconfig eth0 down
 else
     echo "pppwn execution failed. Exiting script."
     exit 1
